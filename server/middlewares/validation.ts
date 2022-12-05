d1fb216eedd0ac00b93e07906a48dc96920006cc
Sérgio Righi <sergiohrighi@gmail.com>
2022-12-04 21:12:16 -0500

    server mored to a new repository;

--- server/middlewares/validation.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-/**
- * it validates the existance of the ID property
- */
-
-export default (req: any, res: any, next: any) => {
-  "id" in req.params && !mongoose.isValidObjectId(req.params.id) ? res.status(200).json(null) : next()
-};
